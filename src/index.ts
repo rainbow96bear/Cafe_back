dd5719c005c6c254beca7b44eabeddd8f8da08ed
KimSungJin <rainbow96bear@gmail.com>
2023-05-18 23:07:50 +0900

    ts를 이용하여 express 서버 베이스 작성

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import session from "express-session";
+import path from "path";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app: Express = express();
+app.set("port", process.env.PORT || 8080);
+app.use("/", express.static(path.join(__dirname, "build")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (process.env.NODE_ENV == "production") {
+    morgan("combined")(req, res, next);
+  } else {
+    morgan("dev")(req, res, next);
+  }
+});
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    secret: String(process.env.COOKIE_SECRET),
+    name: "session",
+  })
+);
+app.listen(app.get("port"), () => {
+  console.log("서버 열음");
+});
